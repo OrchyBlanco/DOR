377d8edf8f3b26eba3a90a3911230114c5d0ac79
Jejormi <jejormi@gmail.com>
2017-01-22 19:26:17 +0000

    Ejercicio svg

--- UD5. Contenidos Multimedia/Ejercicios/UD5T3/ud5t3.js
+++ UD5. Contenidos Multimedia/Ejercicios/UD5T3/ud5t3.js
@@ -1,18 +1,33 @@
+var svgns="http://www.w3.org/2000/svg";
 var cajaSVG=document.getElementById('svg');
 var cx=20;
 var cy=20;
 var r=19;
 
-var circulo=document.createElement("CIRCLE");
-circulo.cx=cx;
-circulo.cy=cy;
-circulo.r=r;
-  for (var i = 0; i < 4; i++) {
+var numeroReservas= document.getElementById('nReservas').value;
+for (var x = 50; x < 250; x += 50) {
+    for (var y = 50; y < 250; y += 50) {
+        var circle = document.createElementNS(svgns, 'circle');
+        circle.setAttributeNS(null, 'cx', x);
+        circle.setAttributeNS(null, 'cy', y);
+        circle.setAttributeNS(null, 'r', r);
+        circle.classList.add("noAsignado");
 
-      circulo.cx+=cx;
-      circulo.cy+=cy;
-      circulo.r=r;
-      circulo.classList.add("reserva");
-      cajaSVG.appendChild(circulo);
+        if (x==50&&y==50) {
+          circle.classList.remove("noAsignado");
+          circle.classList.add("reserva");
+        }
+       if (circle.classList.contains("noAsignado")) {
+         circle.addEventListener("click",function() {
+           this.classList.remove("noAsignado");
+           this.classList.remove("Asignado");
+         });
+        }
+        cajaSVG.appendChild(circle);
+    }
+}
 
-  }
+function cambiarAsignado(e) {
+  this.classList.remove("noAsignado");
+  this.classList.remove("Asignado");
+}
