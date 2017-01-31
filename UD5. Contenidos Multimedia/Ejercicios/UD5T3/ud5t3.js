e01b93e98076611f0086dfb1167e3a7e6807a589
Orchyblanco <jejormi@gmail.com>
2017-01-31 13:11:20 +0000

    Añadidas actividades 3 y 4 del tema 5

--- UD5. Contenidos Multimedia/Ejercicios/UD5T3/ud5t3.js
+++ UD5. Contenidos Multimedia/Ejercicios/UD5T3/ud5t3.js
@@ -4,83 +4,146 @@ var cx = 20;
 var cy = 20;
 var r = 19;
 
-var nButacas=4;
-var svgNButacas=50*nButacas;
-var butaca=50;
+var nButacas = 4;
+var svgNButacas = 70 * nButacas;
+var butaca = 70;
 
 var nReservas = document.getElementById('nReservas').value;
 var buttonRefresco = document.getElementById('refrescar');
 var reservasAsignadas = 0;
 
+var txtButacas = document.getElementById('txtJson');
+
+//--------JSON-------------//
+//A:Asignado R:Reservado L: Libre
+
+var Butacas = {
+    "filas": [{
+            "fila": ["L", "R", "L", "R"]
+        },
+        {
+            "fila": ["R", "R", "L", "L"]
+        },
+        {
+            "fila": ["L", "L", "R", "L"]
+        },
+        {
+            "fila": ["L", "L", "L", "L"]
+        }
+    ]
+};
+
+//------------------------//
+txtButacas.value = JSON.stringify(Butacas.filas);
+setInterval(function() {
+
+    jsonToSvg();
+    pintarButacas();
+},500);
+
+function jsonToSvg() {
+    Butacas.filas=JSON.parse(txtButacas.value);
+}
+
+function MostrarDatos() {
+    var resultado = "";
+    for (var i in Butacas.filas) {
+      resultado+="\n";
+        for (var j in Butacas.filas[i].fila) {
+        resultado += Butacas.filas[i].fila[j] ;
+
+    }}
+    return resultado;
+}
+
+
+
 buttonRefresco.addEventListener("click", function() {
     nReservas = document.getElementById('nReservas').value;
     console.log(nReservas);
 });
-for (var x = butaca; x <= svgNButacas; x += butaca) {
-    for (var y = butaca; y <= svgNButacas; y += butaca) {
-        var circle = document.createElementNS(svgns, 'circle');
-        circle.setAttributeNS(null, 'cx', x);
-        circle.setAttributeNS(null, 'cy', y);
-        circle.setAttributeNS(null, 'r', r);
-        circle.classList.add("noAsignado");
-        circle.id=""+x/50+"|"+y/50+"";
-        pintarAsignados(x,y);
-        if (circle.classList.contains("noAsignado") || circle.classList.contains("Asignado")) {
+function pintarButacas() {
+reservasAsignadas=0;
+  for (var x = butaca; x <= svgNButacas; x += butaca) {
+      for (var y = butaca; y <= svgNButacas; y += butaca) {
+          var circle = document.createElementNS(svgns, 'circle');
+          circle.setAttributeNS(null, 'cx', x + 60);
+          circle.setAttributeNS(null, 'cy', y + 90);
+          circle.setAttributeNS(null, 'r', r);
+          circle.id = "" + x / butaca + "|" + y / butaca + "";
 
-            circle.addEventListener("click", cambiarAsignado);
-        }
-        cajaSVG.appendChild(circle);
-    }
+          switch (Butacas.filas[y/70-1].fila[x/70-1]) {
+              case "L":
+                  circle.classList.add("noAsignado");
+                  circle.classList.remove("Asignado");
+                  circle.classList.remove("reserva");
+                  break;
+              case "A":
+                  circle.classList.add("Asignado");
+                  reservasAsignadas++;
+                  circle.classList.remove("noAsignado");
+                  circle.classList.remove("reserva");
+                  break;
+              case "R":
+                  circle.classList.add("reserva");
+                  circle.classList.remove("Asignado");
+                  circle.classList.remove("noAsignado");
+                  break;
+              default:
+              circle.classList.add("noAsignado");
+              circle.classList.remove("Asignado");
+              circle.classList.remove("reserva");
+          }
+          /*switch (circle.classList[0]) {
+            case "noAsignado":
+                Butacas.filas[y/70-1].fila[x/70-1]="L";
+                break;
+            case "Asignado":
+                Butacas.filas[y/70-1].fila[x/70-1]="A";
+                break;
+            case "reserva":
+                Butacas.filas[y/70-1].fila[x/70-1]="R";
+                break;
+            default:
+          }
+          */
+
+          if (circle.classList.contains("noAsignado") || circle.classList.contains("Asignado")) {
+
+              circle.addEventListener("click", cambiarAsignado);
+          }
+          cajaSVG.appendChild(circle);
+      }
+  }
 }
 
-function cambiarAsignado(e) {
-
-        if (nReservas !== "" &&
-            reservasAsignadas < nReservas &&
-            this.classList.contains("noAsignado")) {
-            this.classList.remove("noAsignado");
-            this.classList.add("Asignado");
-            reservasAsignadas++;
-
+function cambiarAsignado() {
+    if (nReservas !== "" &&
+        reservasAsignadas < nReservas &&
+        this.classList.contains("noAsignado")) {
+        this.classList.remove("noAsignado");
+        this.classList.add("Asignado");
+        reservasAsignadas++;
     } else if (this.classList.contains("Asignado")) {
         this.classList.add("noAsignado");
         this.classList.remove("Asignado");
         reservasAsignadas--;
     }
-
-
 }
 
-function pintarAsignados(x,y) {
-  var rnd1=Math.floor((Math.random() * 4) + 1);
-  var rnd2=Math.floor((Math.random() * 4) + 1);
-  var rnd3=Math.floor((Math.random() * 16) + 1);
-  for (var i = 0; i <= rnd3; i++) {
-    if (x == butaca*rnd1 && y == butaca*rnd2&& circle.classList.contains("noAsignado") ) {
-        circle.classList.remove("noAsignado");
-        circle.classList.add("reserva");
+function pintarJSON(elm,x,y) {
 
-    }
-    if (circle.classList.contains("Asignado")) {
-      i--;
-    }
-    rnd1=Math.floor((Math.random() * 4) + 1);
-    rnd2=Math.floor((Math.random() * 4) + 1);
-  }
-/*  if (x == butaca*1 && y == butaca*1) {
-      circle.classList.remove("noAsignado");
-      circle.classList.add("reserva");
-  }
-  if (x == butaca*2 && y == butaca*1) {
-      circle.classList.remove("noAsignado");
-      circle.classList.add("reserva");
-  }
-  if (x ==butaca*2 && y == butaca*2) {
-      circle.classList.remove("noAsignado");
-      circle.classList.add("reserva");
-  }
-  if (x == butaca*4 && y == butaca*4) {
-      circle.classList.remove("noAsignado");
-      circle.classList.add("reserva");
-  }*/
+            switch (Butacas.filas[x-1].fila[y-1]) {
+                case "L":
+                    circle.className= "noAsignado";
+                    break;
+                case "A":
+                    circle.className = "Asignado";
+                    break;
+                case "R":
+                    circle.className = "Reservado";
+                    break;
+                default:
+            }
+
 }
